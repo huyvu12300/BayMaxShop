a8bcd5110d6e8eae7c4249bea0666cf2e5fa3b3f
nguyentrungne <114131139+nguyentrungne@users.noreply.github.com>
2023-04-17 22:50:27 +0700

    Update by Trung

--- BayMaxShop/BayMaxShop/Areas/Admin/Controllers/NewsController.cs
+++ BayMaxShop/BayMaxShop/Areas/Admin/Controllers/NewsController.cs
@@ -1,5 +1,6 @@
 ﻿using BayMaxShop.Models;
 using BayMaxShop.Models.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,30 +13,114 @@ namespace BayMaxShop.Areas.Admin.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Admin/News
-        public ActionResult Index()
+        public ActionResult Index(string Searchtext, int? page)
         {
-            var items = db.News.OrderByDescending(x => x.Title).ToList();
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<News> items = db.News.OrderByDescending(x => x.Id);
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+
+                items = items.Where(x => x.Alias.Contains(Searchtext) || x.Title.Contains(Searchtext));
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
             return View(items);
         }
         public ActionResult Add()
         {
-            return View(); 
+            return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Add(News model) 
+        public ActionResult Add(News model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
+                model.CreatedDate = DateTime.Now;
                 model.CategoryId = 3;
-                model.CreatedDate= DateTime.Now;
-                model.ModifiedDate= DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
                 model.Alias = BayMaxShop.Models.Commons.Filter.FilterChar(model.Title);
                 db.News.Add(model);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var item = db.News.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                // cái này chừng nào đủ danh mục thì sẽ thay đổi sau
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = BayMaxShop.Models.Commons.Filter.FilterChar(model.Title);
+                db.News.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.News.Find(id);
+            if (item != null)
+            {
+                db.News.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false });
+        }
+        [HttpPost]
+        public ActionResult IsActive(int id)
+        {
+            var item = db.News.Find(id);
+            if (item != null)
+            {
+                item.IsActive = !item.IsActive;
+                db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, isActive = item.IsActive });
+            }
+
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.News.Find(Convert.ToInt32(item));
+                        db.News.Remove(obj);
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+
         }
     }
 }
